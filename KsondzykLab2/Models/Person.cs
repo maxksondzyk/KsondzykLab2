c890af51a118a0e746aeeb08fd58cf69d58bf8dc
Max <maxksiondzyk@gmail.com>
2020-02-22 12:15:41 +0200

    Initial commit with a person class

---
+++ KsondzykLab2/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KsondzykLab2.Models
+{
+    internal class Person
+    {
+        private string _name;
+        private string _lastName;
+        private string _mail;
+        private DateTime _birthday;
+
+        public Person(string name, string lastName, string mail, DateTime birthday)
+        {
+            this._name = name;
+            this._lastName = lastName;
+            this._mail = mail;
+            this._birthday = birthday;
+        }
+        public Person(string name, string lastName, string mail)
+        {
+            this._name = name;
+            this._lastName = lastName;
+            this._mail = mail;
+        }
+        public Person(string name, string lastName, DateTime birthday)
+        {
+            this._name = name;
+            this._lastName = lastName;
+            this._birthday = birthday;
+        }
+
+        private readonly bool IsAdult;
+        private readonly string SunSiign;
+        private readonly string ChineseSign;
+        private readonly bool isBirthday;
+
+    }
+}

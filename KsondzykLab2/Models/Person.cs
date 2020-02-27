30054aea095a0b620feb39bbed2c5254d0c19ffd
Max <maxksiondzyk@gmail.com>
2020-02-27 13:39:44 +0200

    The working version

--- KsondzykLab2/Models/Person.cs
+++ KsondzykLab2/Models/Person.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace KsondzykLab2.Models
 {
@@ -9,32 +7,127 @@ namespace KsondzykLab2.Models
         private string _name;
         private string _lastName;
         private string _mail;
-        private DateTime _birthday;
-
-        public Person(string name, string lastName, string mail, DateTime birthday)
+        private DateTime? _birthday;
+        public readonly string IsAdult;
+        public readonly string SunSign;
+        public readonly string ChineseSign;
+        public readonly string isBirthday;
+        public Person(string name, string lastName, string mail, DateTime? birthday)
         {
             this._name = name;
             this._lastName = lastName;
             this._mail = mail;
             this._birthday = birthday;
+
+            isBirthday = BirthdayCalculate();
+            SunSign = SunSignCalculate();
+            ChineseSign = ChineseSignCalculate();
+            IsAdult = AdultCalculate();
         }
-        public Person(string name, string lastName, string mail)
+
+        public Person(string name = " ", string lastName = " ", string mail = " ")
         {
             this._name = name;
             this._lastName = lastName;
             this._mail = mail;
+
         }
-        public Person(string name, string lastName, DateTime birthday)
+
+        public Person(string name, string lastName, DateTime? birthday)
         {
             this._name = name;
             this._lastName = lastName;
             this._birthday = birthday;
+
+            isBirthday = BirthdayCalculate();
+            SunSign = SunSignCalculate();
+            ChineseSign = ChineseSignCalculate();
+            IsAdult = AdultCalculate();
         }
 
-        private readonly bool IsAdult;
-        private readonly string SunSiign;
-        private readonly string ChineseSign;
-        private readonly bool isBirthday;
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
 
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Mail
+        {
+            get => _mail;
+            set => _mail = value;
+        }
+
+        public DateTime? Birthday
+        {
+            get => _birthday;
+            set => _birthday = value;
+        }
+        private string ChineseSignCalculate()
+        {
+            var result = (_birthday.Value.Year % 12) switch
+            {
+                4 => "Rat",
+                5 => "Ox",
+                6 => "Tiger",
+                7 => "Rabbit",
+                8 => "Dragon",
+                9 => "Snake",
+                10 => "Horse",
+                11 => "Goat",
+                0 => "Monkey",
+                1 => "Rooster",
+                2 => "Dog",
+                3 => "Pig"
+            };
+            return result;
+        }
+
+        private string SunSignCalculate()
+        {
+            var day = _birthday.Value.Day;
+            var result = _birthday.Value.Month switch
+            {
+                3 => (day >= 21 ? "Pisces" : "Aries"),
+                4 => (day <= 20 ? "Aries" : "Taurus"),
+                5 => (day <= 21 ? "Taurus" : "Gemini"),
+                6 => (day <= 21 ? "Gemini" : "Cancer"),
+                7 => (day <= 22 ? "Cancer" : "Leo"),
+                8 => (day <= 23 ? "Leo" : "Virgo"),
+                9 => (day <= 23 ? "Virgo" : "Libra"),
+                10 => (day <= 23 ? "Libra" : "Scorpio"),
+                11 => (day <= 22 ? "Scorpio" : "Sagitarius"),
+                12 => (day <= 21 ? "Sagitarius" : "Capricorn"),
+                1 => (day <= 20 ? "Capricorn" : "Aquarius"),
+                2 => (day <= 18 ? "Aquarius" : "Pisces"),
+            };
+            return result;
+        }
+
+        private string BirthdayCalculate()
+        {
+            if (DateTime.Today.DayOfYear.Equals(_birthday.Value.DayOfYear))
+                return "true";
+
+            return "false";
+        }
+
+        private string AdultCalculate()
+        {
+            var leapYears = (DateTime.Now.Year - this._birthday.Value.Year) / 4;
+            var leapDays = leapYears * 366;
+            var timeSpan = (DateTime.Today - _birthday.Value.Date);
+            var totalDays = timeSpan.Days;
+            totalDays -= leapDays;
+            var years = leapYears + totalDays / 365;
+            if (years > 135 || timeSpan.Days < 0)
+                return "wrong";
+            return years >= 18 ? "true":"false";
+        }
     }
 }
---
+++ KsondzykLab2/ViewModels/ViewModel.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using KsondzykLab2.Models;
+using KsondzykLab2.Tools.Managers;
+using KsondzykLab2.Tools.MVVM;
+
+namespace KsondzykLab2.ViewModels
+{
+
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private bool _filledName;
+        private bool _filledLastName;
+        private bool _filledMail;
+        private bool _filledBirthday;
+        private RelayCommand<object> _startCommand;
+        private Person _person = new Person();
+        private string _name;
+        private string _lastName;
+        private string _mail;
+        private string _isAdult;
+        private string _sunSign;
+        private string _chineseSign;
+        private string _isBirthday;
+        private DateTime? _birthday;
+        private string _birth;
+
+        public DateTime? Birthday
+        {
+            get => _birthday;
+            set
+            {
+                _birthday = value;
+                if (value != null)
+                {
+                    _birth = value.Value.ToShortDateString();
+                    _filledBirthday = true;
+                }
+                else _filledBirthday = false;
+                OnPropertyChanged();
+            }
+        }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                if (value != "")
+                    _filledName = true;
+                else _filledName = false;
+                OnPropertyChanged();
+            }
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                if (value != "")
+                    _filledLastName = true;
+                else _filledLastName = false;
+                OnPropertyChanged();
+            }
+        }
+        public string Mail
+        {
+            get => _mail;
+            set
+            {
+                _mail = value;
+                if (value != "")
+                    _filledMail = true;
+                else _filledMail = false;
+                OnPropertyChanged();
+            }
+        }
+
+        public string IsAdult
+        {
+            get
+            {
+                return _isAdult;
+            }
+            set
+            {
+                _isAdult = value;
+                OnPropertyChanged();
+            }
+        }
+        public string IsBirthday
+        {
+            get
+            {
+                return _isBirthday;
+            }
+            set
+            {
+                _isBirthday = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SunSign
+        {
+            get
+            {
+                return _sunSign;
+            }
+            set
+            {
+                _sunSign = value;
+                OnPropertyChanged();
+            }
+        }
+        public string ChineseSign
+        {
+            get
+            {
+                return _chineseSign;
+            }
+            set
+            {
+                _chineseSign = value;
+                OnPropertyChanged();
+            }
+        }
+        public string UserMail
+        {
+            get
+            {
+                return _person.Mail;
+            }
+            set
+            {
+                _person.Mail = value;
+                OnPropertyChanged();
+            }
+        }
+        public string UserName
+        {
+            get
+            {
+                return _person.Name;
+            }
+            set
+            {
+                _person.Name = value;
+                OnPropertyChanged();
+            }
+        }
+        public string UserLastName
+        {
+            get
+            {
+                return _person.LastName;
+            }
+            set
+            {
+                _person.LastName = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string Birth
+        {
+            get
+            {
+                return _birth;
+            }
+            set
+            {
+                _birth = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool CanExecute()
+        {
+            return _filledMail&&_filledBirthday&&_filledLastName&&_filledName;
+        }
+        public RelayCommand<object> StartCommand
+        {
+            get
+            {
+                return _startCommand ??= new RelayCommand<object>(Calculate, o => CanExecute());
+            }
+
+        }
+        
+        private async void Calculate(object obj)
+        {
+            
+            LoaderManager.Instance.ShowLoader();
+            await Task.Run(() => Thread.Sleep(2000));
+            _person = new Person(Name,LastName,Mail,Birthday);
+            LoaderManager.Instance.HideLoader();
+            if (_person.IsAdult=="wrong")
+            {
+                MessageBox.Show("Incorrect date!");
+                IsAdult = "";
+                SunSign = "";
+                ChineseSign = "";
+                IsBirthday = "";
+                UserName = "";
+                UserLastName = "";
+                Birth = "";
+                UserMail = "";
+            }
+            else
+            {
+                IsAdult = $"Adult: {_person.IsAdult}";
+                SunSign = $"Your sun sign: {_person.SunSign}";
+                ChineseSign = $"Your chinese sign: {_person.ChineseSign}";
+                IsBirthday = $"It's your birthday: {_person.isBirthday}";
+
+                UserName = $"Your name is {_person.Name}";
+                UserLastName = $"Your last name is {_person.LastName}";
+                Birth = $"Your birthday is: {_person.Birthday.Value.ToShortDateString()}";
+                UserMail = $"Your mail is: {_person.Mail}";
+                if (_person.Birthday.Value.DayOfYear.Equals(DateTime.Today.DayOfYear))
+                {
+                    MessageBox.Show("Happy Birthday!");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
